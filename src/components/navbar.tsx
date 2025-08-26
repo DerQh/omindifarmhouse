ddc1a4bd1635fe6eb75124eb18a8e71b60a7570e
DerQh <64221410+DerQh@users.noreply.github.com>
2025-08-26 08:06:22 +0300

    change 4

---
+++ src/components/navbar.tsx
@@ -0,0 +1,13 @@
+
+
+
+function Navbar(){
+    return (
+        <div>
+            <h1>We bring the storeto your doorstep</h1>
+            <p>Get organic produc and sustainably sourced groceries delivered at up to 4% off grocery</p>
+        </div>
+    )
+}
+
+export default Navbar
