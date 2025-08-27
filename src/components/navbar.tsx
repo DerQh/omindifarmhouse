7c86b1242bb6b384426f1195ea1d8d7ea75f5e52
DerQh <64221410+DerQh@users.noreply.github.com>
2025-08-27 15:16:17 +0300

    day 2-1

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -4,6 +4,7 @@
 function LandingPage(){
     return (
         <>
+        // The main landing page 
         <div>
             <h1>We bring the store to your doorstep</h1>
             <p>Get organic products and sustainably sourced groceries delivered to you all over ALendu and it's environs</p>
@@ -11,18 +12,50 @@ function LandingPage(){
         <div>
             <button> Shop Now</button>
         </div>
+
         <div>
             <img src="" alt="Delivery person holding groceries " />
         </div>
+
+        // The second page
         <div>
             <div>
                  <img src="Free delivery icon" alt="" />
                  <p>Free delivery in Alendu and it's environs</p>
+                 <p>Support 24/7</p>
             </div>
             <div>
+                <img src="" alt="Cash image" />
+                <p>Pay with M-Pesa or cash on delivery</p>
+                <p>Gurantee amd Refunds </p>
+            </div>
+
+            <div>
+                <img src="" alt="Organic Icon" />
+                <p>100% Organc products</p>
+                <p>Secure your future health</p>
             </div>
         </div>
+
+        // Display of random available products
+        <div>
+            <h2>Green & Fresh Products</h2>
+        
+        //Single Pproduct divs
+            <div>
+                <div>
+                    <div>
+                         <button>-Ksh 5</button>
+                    </div>
+                    <img src="" alt="product image" />
+                    <p>Product Description</p>
+                    <p>Ksh 50</p>
+                    <button>Add to Cart</button>
+                </div>
+            </div>
            
+        </div>
+
         </>
     )
 }
