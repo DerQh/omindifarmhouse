e53baa19465eba465a90dfc240c51e6f53ba7be6
DerQh <64221410+DerQh@users.noreply.github.com>
2025-09-01 11:22:13 +0000

    product & checkout pages

---
+++ src/components/GettingStarted.tsx
@@ -0,0 +1,14 @@
+function GettingStarted(){
+
+    return
+    <div>
+        <img src="" alt="basket image" />
+        <div>
+            <p>Fresh Items offers</p>
+            <p>We try our best to make sure our customer gets value out of their money</p>
+        </div>
+        <button>GET STARTED</button>
+    </div>
+}
+
+export default GettingStarted
